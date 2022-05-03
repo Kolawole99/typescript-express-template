339859304f808bd3536776ac494aaa8f80aa174d
Kolawole Ojo <philliphopeojo@gmail.com>
2022-05-03 22:51:41 +0100

    Adding support for passing in environement variables

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
         ecmaVersion: 'latest',
         sourceType: 'module',
     },
-    plugins: ['@typescript-eslint', 'prettier'],
+    plugins: ['@typescript-eslint'],
     rules: {
         'prettier/prettier': 'error',
     },
