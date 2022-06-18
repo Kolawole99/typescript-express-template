c62f3932298b7aa53f60a220fd09c97702ffb5e5
Kolawole Ojo <philliphopeojo@gmail.com>
2022-06-18 01:28:22 +0100

    Adding support for controllers in services

---
+++ src/types/Types.d.ts
@@ -0,0 +1,17 @@
+import Express from 'express';
+
+import { BaseController } from '../controllers/index';
+
+declare global {
+    type Request = Express.Request;
+    type Response = Express.Response;
+    type NextFunction = Express.NextFunction;
+
+    type RequestHandler = {
+        request: Request;
+        response: Response;
+        next: NextFunction;
+    };
+
+    type Controller = BaseController;
+}
