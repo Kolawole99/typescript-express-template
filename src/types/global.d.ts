e16e1e76e0a94b360506ee94c890de1091e3b5d0
Kolawole Ojo <philliphopeojo@gmail.com>
2022-08-13 21:12:28 +0100

    Updates on types in application

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,7 +1,25 @@
-import { Kafka, Redis } from '../utilities/PackageWrapper';
+import BaseController from '../controllers/index';
+import { Express, Kafka, Redis } from '../utilities/PackageWrapper';
 
 declare global {
     type ArrayOfStrings = Array<string>;
+
+    type Controller = BaseController;
+
+    interface DBController {
+        createRecord(request: any): Promise<any>;
+        createRecords(request: any): Promise<any>;
+        readRecord(request: any): Promise<any>;
+        readRecords(request: any): Promise<any>;
+        countRecords(request: any): Promise<any>;
+        updateRecord(request: any): Promise<any>;
+        updateRecords(request: any): Promise<any>;
+        deleteRecord(request: any): Promise<any>;
+        deleteRecords(request: any): Promise<any>;
+    }
+    type DictionaryKeyStringPair = {
+        [key: string]: string;
+    };
 
     /** Kafka typings */
     type Kafka = Kafka.Kafka;
@@ -17,7 +35,19 @@ declare global {
     type RedisClientType = ReturnType<typeof Redis.createClient>;
     type RedisClientOptions = Parameters<typeof Redis.createClient>[0];
 
+    /** Express typings */
+    type NextFunction = Express.NextFunction;
+    type Response = Express.Response;
+    type Request = Express.Request;
+    type RequestHandler = {
+        request: Request;
+        response: Response;
+        next: NextFunction;
+    };
+
     type responseBody = string | object;
+
+    type StringObjectType = string | object;
 }
 
 export {};
