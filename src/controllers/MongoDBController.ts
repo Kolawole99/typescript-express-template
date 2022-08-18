8bc06e80b96afde06d8790cf67aef669ffefc47f
Kolawole Ojo <philliphopeojo@gmail.com>
2022-08-18 08:33:32 +0100

    Integrating mongoDB into the project, using the controller

--- src/controllers/MongoDBController.ts
+++ src/controllers/MongoDBController.ts
@@ -15,6 +15,11 @@ class MongoDBController implements IDBController {
         this.init(modelName);
     }
 
+    /**
+     * This method initializes the Model controller by:
+     * 1. Verifying that the model is registered in the application.
+     * 2. Retrieving the MongoDB instance of the Model using Mongoose so we can get the required methods.
+     */
     private init(modelName: string): void {
         try {
             if (!modelName || !Constants.AppModels.MongoDB[modelName]) {
@@ -27,75 +32,339 @@ class MongoDBController implements IDBController {
         }
     }
 
-    public async createRecord(payload: any): Promise<any> {}
+    private mongoErrorMessage({ error }: { error: Error }) {
+        // const { name, code, keyPattern } = error;
 
-    public async createRecords(payload: any): Promise<any> {}
+        // if (name === 'MongoServerError' && code && code === 11000) {
+        //     const keys = Object.keys(keyPattern).join(',');
 
+        //     return `Entry already exist for ${keys} `;
+        // } else if (name === 'ValidationError') {
+        //     if (error.errors[Object.keys(error.errors)[0]].properties) {
+        //         const { type, path } = error.errors[Object.keys(error.errors)[0]].properties;
+
+        //         return `Validation failed for field ${path} (${type}) `;
+        //     }
+        //     const { kind, value, path } = error.errors[Object.keys(error.errors)[0]];
+
+        //     return `Validation failed for field ${path} (${value}) instead of ${kind} `;
+        // }
+
+        return 'Validation failed';
+    }
+
+    /**
+     * Local method to process all class errors.
+     */
+    private processError(error: Error) {
+        const message = this.mongoErrorMessage({ error }) || error.message;
+
+        return { failed: true, error: message };
+    }
+
+    /** This method accepts an object and uses it to creates a record */
+    public async createRecord(data: object): Promise<any> {
+        try {
+            const record = await this.model.create(data);
+
+            return parseObject(record);
+        } catch (error) {
+            return error;
+        }
+    }
+
+    /** This method accepts an array of objects and uses it to creates multiple records */
+    public async createRecords(data: ArrayOfObjects): Promise<any> {
+        try {
+            const record = await this.model.create(data);
+
+            return parseObject(record);
+        } catch (error) {
+            return error;
+        }
+    }
+
+    /** This method fetches the first document matching the provided filter */
     public async readRecord(data: object): Promise<object | unknown> {
         try {
-            const record = await this.model.findOne(data).setOptions({ sanitizeFilter: true });
+            const record: object = await this.model
+                .findOne(data)
+                .setOptions({ sanitizeFilter: true });
 
-            if (record) {
-                return parseObject(record);
-            }
+            return parseObject(record);
         } catch (error) {
             return error;
         }
     }
 
+    /** This method takes in a filter and some properties such as skip, limit and returns all matching documents  */
     public async readRecords(payload: {
         data: object;
-        skip: number;
-        limit: number;
-    }): Promise<number | unknown> {
+        fieldsToReturn?: string | object;
+        sortOptions?: string;
+        skip?: number;
+        limit?: number;
+    }): Promise<object | unknown> {
         try {
-            const { data, skip, limit } = payload;
+            const {
+                data,
+                fieldsToReturn = '',
+                sortOptions = '',
+                skip = 0,
+                limit = Number.MAX_SAFE_INTEGER,
+            } = payload;
 
-            const record = await this.model
-                .find(data)
+            const record: any[] = await this.model
+                .find({ ...data }, fieldsToReturn)
                 .setOptions({ sanitizeFilter: true })
                 .skip(skip)
-                .limit(limit);
+                .limit(limit)
+                .sort(sortOptions);
 
-            if (record) {
-                return parseObject(record);
-            }
+            return parseObject(record);
         } catch (error) {
             return error;
         }
     }
 
-    public async aggregateRecord(payload: any): Promise<any> {}
-
-    public async countRecords(payload: {
-        data: object;
-        skip: number;
-        limit: number;
-    }): Promise<number | unknown> {
+    /** This method counts all the document in this collection that matches the provided filter */
+    public async countRecordsByFilter(data: object): Promise<object | unknown> {
         try {
-            const { data, skip, limit } = payload;
-
-            return await this.model
-                .countDocuments(data)
-                .setOptions({ sanitizeFilter: true })
-                .skip(skip)
-                .limit(limit);
+            return {
+                count: await this.model.countDocuments(data).setOptions({ sanitizeFilter: true }),
+            };
         } catch (error) {
             return error;
         }
     }
 
-    public async updateRecord(payload: any): Promise<any> {}
+    /** This method counts all the document in this collection. This does not accept a filter */
+    public async countAllModelRecords(): Promise<object | unknown> {
+        try {
+            return {
+                count: await this.model.estimatedDocumentCount(),
+            };
+        } catch (error) {
+            return error;
+        }
+    }
 
-    public async updateRecords(payload: any): Promise<any> {}
+    /** This method updates the first document matching the provided filter */
+    public async updateRecord(payload: { conditions: object; dataToSet: object }): Promise<any> {
+        try {
+            const { conditions, dataToSet } = payload;
 
-    public async softDeleteRecord(payload: any): Promise<any> {}
+            const result = await this.model
+                .updateOne(
+                    { ...conditions },
+                    {
+                        ...dataToSet,
+                        $currentDate: { updatedOn: true },
+                    }
+                )
+                .setOptions({ sanitizeFilter: true });
 
-    public async softDeleteRecords(payload: any): Promise<any> {}
+            return parseObject(result);
+        } catch (error) {
+            return error;
+        }
+    }
 
-    public async hardDeleteRecord(payload: any): Promise<any> {}
+    /** This method updates all the documents matching the provided filter */
+    public async updateRecords(payload: { conditions: object; dataToSet: object }): Promise<any> {
+        try {
+            const { conditions, dataToSet } = payload;
 
-    public async hardDeleteRecords(payload: any): Promise<any> {}
+            const result = await this.model
+                .updateMany(
+                    { ...conditions },
+                    {
+                        ...dataToSet,
+                        $currentDate: { updatedOn: true },
+                    }
+                )
+                .setOptions({ sanitizeFilter: true });
+
+            return parseObject(result);
+        } catch (error) {
+            return error;
+        }
+    }
+
+    /**
+     * This method overwrites the total values of the first document matching the query completely with the new data.
+     * Only use this method when you do not care about the previous data stored for the filter(that is you want to upsert the previous value).
+     */
+    public async updateRecordByReplacing(payload: {
+        conditions: object;
+        dataToSet: object;
+    }): Promise<any> {
+        try {
+            const { conditions, dataToSet } = payload;
+
+            const result = await this.model
+                .replaceOne(
+                    { ...conditions },
+                    {
+                        ...dataToSet,
+                        $currentDate: { updatedOn: true },
+                    }
+                )
+                .setOptions({ sanitizeFilter: true });
+
+            return parseObject(result);
+        } catch (error) {
+            return error;
+        }
+    }
+
+    /**
+     * This method sets "isActive = false" for the first document matching the provided filter.
+     *
+     * This is a form of in application Trash where you can send documents to.
+     * We advise you use this always for easy data restoration.
+     */
+    public async softDeleteRecord(conditions: object): Promise<any> {
+        try {
+            const result = await this.model
+                .updateOne(
+                    { ...conditions },
+                    {
+                        isActive: false,
+                        isDeleted: false,
+                        $currentDate: { updatedOn: true },
+                    }
+                )
+                .setOptions({ sanitizeFilter: true });
+
+            return parseObject(result);
+        } catch (error) {
+            return error;
+        }
+    }
+
+    /**
+     * This method sets "isActive = false" for all the documents matching the provided filter.
+     *
+     * This is a form of in application Trash where you can send documents to.
+     * We advise you use this always for easy data restoration.
+     */
+    public async softDeleteRecords(conditions: object): Promise<any> {
+        try {
+            const result = await this.model
+                .updateMany(
+                    { ...conditions },
+                    {
+                        isActive: false,
+                        isDeleted: false,
+                        $currentDate: { updatedOn: true },
+                    }
+                )
+                .setOptions({ sanitizeFilter: true });
+
+            return parseObject(result);
+        } catch (error) {
+            return error;
+        }
+    }
+
+    /**
+     * This method sets:
+     *
+     *  1. "isActive=false"
+     *  2. "isDeleted=true"
+     *
+     * On the first document matching the provided filter.
+     *
+     * This is a clearing of the application Trash. Data reaches the end of life here.
+     * End users can no longer access this data at this stage although it is still in the database.
+     */
+    public async hardDeleteRecord(conditions: object): Promise<any> {
+        try {
+            const result = await this.model
+                .updateOne(
+                    { ...conditions },
+                    {
+                        isActive: false,
+                        isDeleted: true,
+                        $currentDate: { updatedOn: true },
+                    }
+                )
+                .setOptions({ sanitizeFilter: true });
+
+            return parseObject(result);
+        } catch (error) {
+            return error;
+        }
+    }
+
+    /**
+     * This method sets:
+     *
+     *  1. "isActive=false"
+     *  2. "isDeleted=true"
+     *
+     * On all the documents matching the provided filter.
+     *
+     * This is a clearing of the application Trash. Data reaches the end of life here.
+     * End users can no longer access this data at this stage although it is still in the database.
+     */
+    public async hardDeleteRecords(conditions: object): Promise<any> {
+        try {
+            const result = await this.model
+                .updateMany(
+                    { ...conditions },
+                    {
+                        isActive: false,
+                        isDeleted: true,
+                        $currentDate: { updatedOn: true },
+                    }
+                )
+                .setOptions({ sanitizeFilter: true });
+
+            return parseObject(result);
+        } catch (error) {
+            return error;
+        }
+    }
+
+    /**
+     * This method deletes the first document matching the provided filter from the collection storage.
+     *
+     * This is an actual delete in the storage delete sense, it is a permanent delete.
+     * You better know what you are doing before you use this method, as why would you want to dig up the dead.
+     * In fact, you are advised not to use this method at all unless the world is coming to an end.
+     */
+    public async totalHardDeleteRecord(conditions: object): Promise<any> {
+        try {
+            const result = await this.model
+                .deleteOne(conditions)
+                .setOptions({ sanitizeFilter: true });
+
+            return parseObject(result);
+        } catch (error) {
+            return error;
+        }
+    }
+
+    /**
+     * This method deletes all the documents matching the provided filter from the collection storage.
+     *
+     * This is an actual delete in the storage delete sense, it is a permanent delete.
+     * You better know what you are doing before you use this method, as why would you want to dig up the dead.
+     * In fact, you are advised not to use this method at all unless the world is coming to an end.
+     */
+    public async totalHardDeleteRecords(conditions: object): Promise<any> {
+        try {
+            const result = await this.model
+                .deleteMany(conditions)
+                .setOptions({ sanitizeFilter: true });
+
+            return parseObject(result);
+        } catch (error) {
+            return error;
+        }
+    }
 }
 
 export default MongoDBController;
--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,29 +1,38 @@
 import { Express, Kafka, Mongoose, Redis } from '../utilities/PackageWrapper';
 
 declare global {
+    /** Data structures */
     type ArrayOfStrings = Array<string>;
+    type ArrayOfObjects = Array<object>;
+    type DictionaryKeyStringPair = {
+        [key: string]: string;
+    };
+    type ObjectOfObject = {
+        [key: string]: Object;
+    };
+    type StringObjectType = string | object;
 
+    /** Controller typings */
     interface IDBController {
         createRecord(request: any): Promise<any>;
         createRecords(request: any): Promise<any>;
         readRecord(request: any): Promise<any>;
         readRecords(request: any): Promise<any>;
-        aggregateRecord(request: any): Promise<any>;
-        countRecords(request: any): Promise<any>;
+        countRecordsByFilter(request: any): Promise<any>;
+        countAllModelRecords(request: any): Promise<any>;
         updateRecord(request: any): Promise<any>;
         updateRecords(request: any): Promise<any>;
+        updateRecordByReplacing(request: any): Promise<any>;
         softDeleteRecord(request: any): Promise<any>;
         softDeleteRecords(request: any): Promise<any>;
         hardDeleteRecord(request: any): Promise<any>;
         hardDeleteRecords(request: any): Promise<any>;
+        totalHardDeleteRecord(request: any): Promise<any>;
+        totalHardDeleteRecords(request: any): Promise<any>;
     }
     interface IDBControllerConstructor {
         new (value: string): IDBController;
     }
-
-    type DictionaryKeyStringPair = {
-        [key: string]: string;
-    };
 
     /** Kafka typings */
     type Kafka = Kafka.Kafka;
@@ -35,6 +44,7 @@ declare global {
     };
     interface KafkaTopicConfiguration extends Kafka.ITopicConfig {}
 
+    /** Mongoose and MongoDB typings */
     type MongooseModel = typeof Mongoose.Model;
 
     /** Redis typings */
@@ -52,8 +62,6 @@ declare global {
     };
 
     type responseBody = string | object;
-
-    type StringObjectType = string | object;
 }
 
 export {};
